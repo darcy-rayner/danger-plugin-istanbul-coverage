ce1e286cc2b1a56a540302a455c471122ee4a4d1
Darcy Rayner <darcy@two-bulls.com>
2018-03-05 00:23:40 -0500

    Add test coverage.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,108 @@
-import karmaInstanbul from "./index"
+import { Volume } from "memfs"
+import { karmaInstanbul, ReportChangeType, ReportMode } from "./index"
 
 declare const global: any
+
+const basePath = "/some/random/path/to/repo"
+
+function makeCoverageEntry(coverage) {
+  return `{
+      "0": ${ coverage < 25 ? 0 : 1 },
+      "1": ${ coverage < 50 ? 0 : 1},
+      "2": ${ coverage < 75 ? 0 : 1 },
+      "3":  ${ coverage < 100 ? 0 : 1 }
+    }`
+}
+
+function makeEntry(fileName: string, statementCoverage: number = 100, functionCoverage: number = 100,
+                   branchCoverage: number = 100) {
+  return `
+    "${fileName}: {
+      "path": "${fileName}",
+      "statementMap": {
+        "0": { "start": {"line": 0, "column": 3 }, "end": { "line": 3, "column": 35 } },
+        "1": { "start": {"line": 10, "column": 3 }, "end": { "line": 13, "column": 35 } },
+        "2": { "start": {"line": 20, "column": 3 }, "end": { "line": 23, "column": 35 } },
+        "3": { "start": {"line": 30, "column": 3 }, "end": { "line": 33, "column": 35 } }
+      },
+      "fnMap": {
+        "0": {
+          "name": "someFunction1",
+          "decl": { "start": {"line": 1, "column": 3 }, "end": { "line": 4, "column": 35 } },
+          "loc": { "start": {"line": 1, "column": 3 }, "end": { "line": 4, "column": 35 } },
+        },
+        "1": {
+          "name": "someFunction2",
+          "decl": { "start": {"line": 11, "column": 3 }, "end": { "line": 14, "column": 35 } },
+          "loc": { "start": {"line": 11, "column": 3 }, "end": { "line": 14, "column": 35 } },
+        },
+        "2": {
+          "name": "someFunction3",
+          "decl": { "start": {"line": 21, "column": 3 }, "end": { "line": 24, "column": 35 } },
+          "loc": { "start": {"line": 21, "column": 3 }, "end": { "line": 24, "column": 35 } },
+        },
+        "3": {
+          "name": "someFunction4",
+          "decl": { "start": {"line": 31, "column": 3 }, "end": { "line": 34, "column": 35 } },
+          "loc": { "start": {"line": 31, "column": 3 }, "end": { "line": 34, "column": 35 } },
+        }
+      },
+      "branchMap": {
+        "0": {
+          "loc": {
+            "start": {"line": 2, "column": 3 }, "end": { "line": 5, "column": 35 }
+          },
+          "type": "if",
+          "locations": [
+            { "start": {"line": 2, "column": 3 }, "end": { "line": 5, "column": 35 } },
+            { "start": {"line": 2, "column": 3 }, "end": { "line": 5, "column": 35 } }
+          ]
+        },
+        "1": {
+          "loc": {
+            "start": {"line": 12, "column": 3 }, "end": { "line": 15, "column": 35 }
+          },
+          "type": "if",
+          "locations": [
+            { "start": {"line": 12, "column": 3 }, "end": { "line": 15, "column": 35 } },
+            { "start": {"line": 12, "column": 3 }, "end": { "line": 15, "column": 35 } }
+          ]
+        },
+        "2": {
+          "loc": {
+            "start": {"line": 22, "column": 3 }, "end": { "line": 25, "column": 35 }
+          },
+          "type": "if",
+          "locations": [
+            { "start": {"line": 22, "column": 3 }, "end": { "line": 25, "column": 35 } },
+            { "start": {"line": 22, "column": 3 }, "end": { "line": 25, "column": 35 } }
+          ]
+        },
+        "3": {
+          "loc": {
+            "start": {"line": 32, "column": 3 }, "end": { "line": 35, "column": 35 }
+          },
+          "type": "if",
+          "locations": [
+            { "start": {"line": 32, "column": 3 }, "end": { "line": 35, "column": 35 } },
+            { "start": {"line": 32, "column": 3 }, "end": { "line": 35, "column": 35 } }
+          ]
+        }
+      },
+      "s": ${makeCoverageEntry(statementCoverage)},
+      "f": ${makeCoverageEntry(functionCoverage)},
+      "b": ${makeCoverageEntry(branchCoverage)},
+    }
+  `
+}
+
+function setupCoverageFile(coverage) {
+  const volume = Volume.fromJSON({
+    "./coverage/coverage-final.json": coverage,
+  })
+  jest.setMock("fs", volume)
+}
+
 
 describe("karmaInstanbul()", () => {
   beforeEach(() => {
@@ -8,6 +110,25 @@ describe("karmaInstanbul()", () => {
     global.message = jest.fn()
     global.fail = jest.fn()
     global.markdown = jest.fn()
+    global.danger = {
+      git: {
+        modified_files: [
+          "src/modified-file1.ts",
+          "src/modified-file2.ts",
+        ],
+        created_files: [
+          "src/created-file1.ts",
+          "src/created-file2.ts",
+        ],
+      },
+    }
+    setupCoverageFile(`{
+      ${makeEntry(`${basePath}/src/modified-file1.ts`, 25, 25, 25)},
+      ${makeEntry(`${basePath}/src/modified-file2.ts`, 50, 75, 50)},
+      ${makeEntry(`${basePath}/src/created-file1.ts`, 100, 25, 50)},
+      ${makeEntry(`${basePath}/src/created-file2.ts`, 75, 50, 25)},
+      ${makeEntry(`${basePath}/src/unmodified-field.ts`, 25, 25, 25)},
+    }`)
   })
 
   afterEach(() => {
@@ -15,17 +136,140 @@ describe("karmaInstanbul()", () => {
     global.message = undefined
     global.fail = undefined
     global.markdown = undefined
+    jest.resetAllMocks()
   })
 
-  it("Checks for a that message has been called", () => {
-    global.danger = {
-      github: { pr: { title: "My Test Title" } },
-    }
-
-    karmaInstanbul()
-
-    expect(global.message).toHaveBeenCalledWith(
-      "PR Title: My Test Title",
+  it("will only report on new files when reportChangeType is set to CREATED_FILES", () => {
+    karmaInstanbul({
+      reportChangeType: ReportChangeType.CREATED_FILES,
+    })
+    expect(global.markdown).toHaveBeenCalledWith(
+`## Coverage in New Files
+File | Statement Coverage | Function Coverage | Branch Coverage
+---- | ------------------ | ----------------- | ---------------
+[src/created-file1.ts](src/created-file1.ts) | 100% | 25% | 50%
+[src/created-file2.ts](src/created-file2.ts) | 75% | 50% | 25%
+`,
     )
+  })
+
+  it("will only report on modified files when reportChangeType is set to MODIFIED_FILES", () => {
+    karmaInstanbul({
+      reportChangeType: ReportChangeType.CREATED_FILES,
+    })
+    expect(global.markdown).toHaveBeenCalledWith(
+`## Coverage in New and Modified Files
+File | Statement Coverage | Function Coverage | Branch Coverage
+---- | ------------------ | ----------------- | ---------------
+[src/modified-file1.ts](src/modified-file1.ts) | 25% | 25% | 25%
+[src/modified-file2.ts](src/modified-file2.ts) | 50% | 75% | 50%
+`,
+    )
+  })
+  it("will only report on created and modified files when reportChangeType is set to CREATED_OR_MODIFIED_FILES", () => {
+    karmaInstanbul({
+      reportChangeType: ReportChangeType.CREATED_FILES,
+    })
+    expect(global.markdown).toHaveBeenCalledWith(
+`## Coverage in Modified Files
+File | Statement Coverage | Function Coverage | Branch Coverage
+---- | ------------------ | ----------------- | ---------------
+[src/modified-file1.ts](src/modified-file1.ts) | 25% | 25% | 25%
+[src/modified-file2.ts](src/modified-file2.ts) | 50% | 75% | 50%
+[src/created-file1.ts](src/created-file1.ts) | 100% | 25% | 50%
+[src/created-file2.ts](src/created-file2.ts) | 75% | 50% | 25%
+`,
+    )
+  })
+
+  it("will report all files when reportChangeType is set to ALL_FILES", () => {
+    karmaInstanbul({
+      reportChangeType: ReportChangeType.CREATED_FILES,
+    })
+    expect(global.markdown).toHaveBeenCalledWith(
+`## Coverage in All Files
+File | Statement Coverage | Function Coverage | Branch Coverage
+---- | ------------------ | ----------------- | ---------------
+[src/modified-file1.ts](src/modified-file1.ts) | 25% | 25% | 25%
+[src/modified-file2.ts](src/modified-file2.ts) | 50% | 75% | 50%
+[src/created-file1.ts](src/created-file1.ts) | 100% | 25% | 50%
+[src/created-file2.ts](src/created-file2.ts) | 75% | 50% | 25%
+[src/unmodified-field.ts](src/unmodified-field.ts)| 25% | 25% | 25%
+`,
+    )
+  })
+
+  it("fails the build when reportMode is set to FAIL and coverage is below threshold", () => {
+    karmaInstanbul({
+      reportMode: ReportMode.FAIL,
+    })
+    expect(global.fail).toBeCalled()
+  })
+
+  it("passes the build when reportMode is set to FAIL and coverage is above threshold", () => {
+    karmaInstanbul({
+      reportMode: ReportMode.FAIL,
+      threshold: {
+        statements: 25,
+        functions: 25,
+        branches: 25,
+      },
+    })
+    expect(global.fail).not.toBeCalled()
+  })
+
+  it("warns the build when reportMode is set to WARN and coverage is below threshold", () => {
+    karmaInstanbul({
+      reportMode: ReportMode.WARN,
+    })
+    expect(global.warn).toBeCalled()
+  })
+
+  it("passes the build when reportMode is set to WARN and coverage is above threshold", () => {
+    karmaInstanbul({
+      reportMode: ReportMode.WARN,
+      threshold: {
+        statements: 25,
+        functions: 25,
+        branches: 25,
+      },
+    })
+    expect(global.warn).not.toBeCalled()
+  })
+  it("doesn't output anything when reportChangeType is set to CREATED_FILES and there are no created files ", () => {
+    global.danger.created_files = []
+    karmaInstanbul({
+      reportMode: ReportMode.FAIL,
+      reportChangeType: ReportChangeType.CREATED_FILES,
+    })
+    expect(global.fail).not.toBeCalled()
+    expect(global.message).not.toBeCalled()
+  })
+
+  it("doesn't output anything when reportChangeType is set to MODIFIED_FILES and there are no modified files ", () => {
+    global.danger.modified_files = []
+    karmaInstanbul({
+      reportMode: ReportMode.FAIL,
+      reportChangeType: ReportChangeType.MODIFIED_FILES,
+    })
+    expect(global.fail).not.toBeCalled()
+    expect(global.message).not.toBeCalled()
+  })
+  it("doesn't output anything when there is coverage data is empty", () => {
+    setupCoverageFile("{}")
+    karmaInstanbul({
+      reportMode: ReportMode.FAIL,
+    })
+    expect(global.fail).not.toBeCalled()
+    expect(global.message).not.toBeCalled()
+  })
+  it("outputs a warning when it can't find the coverage file", () => {
+    const volume = Volume.fromJSON({})
+    jest.setMock("fs", volume)
+
+    karmaInstanbul({
+      reportMode: ReportMode.FAIL,
+    })
+    expect(global.warn).toBeCalled()
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,51 @@
 // Provides dev-time type structures for  `danger` - doesn't affect runtime.
 import {DangerDSLType} from "../node_modules/danger/distribution/dsl/DangerDSL"
 declare var danger: DangerDSLType
+import * as fs from "fs"
 export declare function message(message: string): void
 export declare function warn(message: string): void
 export declare function fail(message: string): void
 export declare function markdown(message: string): void
 
-export enum KarmaInstanbulReportMode {
-  NEW_FILES,
+export enum ReportChangeType {
+  CREATED_FILES,
   MODIFIED_FILES,
-  NEW_OR_MODIFIED_FILES,
+  CREATED_OR_MODIFIED_FILES,
+  ALL_FILES,
+}
+export enum ReportMode {
+  FAIL,
+  WARN,
+  MESSAGE,
 }
 
 export interface KarmaInstanbulConfig {
   coveragePath: string,
-  reportMode: KarmaInstanbulReportMode,
+  reportChangeType: ReportChangeType,
   threshold: {
     statements: number,
     lines: number,
     branches: number,
     functions: number,
   },
-  failBuild: boolean
+  reportMode: ReportMode
 }
 
 /**
  * Danger.js plugin for monitoring code coverage on changed files.
  */
-export default function karmaInstanbul(config?: Partial<KarmaInstanbulConfig>) {
+export function karmaInstanbul(config?: Partial<KarmaInstanbulConfig>) {
   const defaults: KarmaInstanbulConfig = {
     coveragePath: "coverage",
-    reportMode: KarmaInstanbulReportMode.NEW_OR_MODIFIED_FILES,
+    reportChangeType: ReportChangeType.CREATED_OR_MODIFIED_FILES,
+    reportMode: ReportMode.MESSAGE,
     threshold: {
       statements: 100,
       lines: 100,
       branches: 100,
       functions: 100,
     },
-    failBuild: false,
   }
   const combinedConfig = config ? { ... defaults, ... config } : defaults
+  throw Error("Unimplemented")
 }
