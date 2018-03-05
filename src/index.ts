8b17d29215ee5dbd55bcb2682246c9fbf27c3c12
Darcy Rayner <darcy@two-bulls.com>
2018-03-04 22:04:00 -0500

    Define interface.

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,38 @@ export declare function warn(message: string): void
 export declare function fail(message: string): void
 export declare function markdown(message: string): void
 
+export enum KarmaInstanbulReportMode {
+  NEW_FILES,
+  MODIFIED_FILES,
+  NEW_OR_MODIFIED_FILES,
+}
+
+export interface KarmaInstanbulConfig {
+  coveragePath: string,
+  reportMode: KarmaInstanbulReportMode,
+  threshold: {
+    statements: number,
+    lines: number,
+    branches: number,
+    functions: number,
+  },
+  failBuild: boolean
+}
+
 /**
  * Danger.js plugin for monitoring code coverage on changed files.
  */
-export default function karmaInstanbul() {
-  // Replace this with the code from your Dangerfile
-  const title = danger.github.pr.title
-  message(`PR Title: ${title}`)
+export default function karmaInstanbul(config?: Partial<KarmaInstanbulConfig>) {
+  const defaults: KarmaInstanbulConfig = {
+    coveragePath: "coverage",
+    reportMode: KarmaInstanbulReportMode.NEW_OR_MODIFIED_FILES,
+    threshold: {
+      statements: 100,
+      lines: 100,
+      branches: 100,
+      functions: 100,
+    },
+    failBuild: false,
+  }
+  const combinedConfig = config ? { ... defaults, ... config } : defaults
 }
