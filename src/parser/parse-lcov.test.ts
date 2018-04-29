5551f7224ff1892a9b438f19b7a4d5667914b30b
Darcy Rayner <darcy.rayner@gmail.com>
2018-04-28 21:53:18 -0400

    test: Add more tests to parse-lcov

--- src/parser/parse-lcov.test.ts
+++ src/parser/parse-lcov.test.ts
@@ -43,4 +43,85 @@ end_of_record`
       },
     })
   })
+
+  it("outputs an empty collection if there is no end_of_record", () => {
+    setupCoverageFile(
+      `TN:
+SF: some/file.ts
+FN: 1, func1
+FNDA: 1, func1
+FNF: 1
+FNH: 1
+BRF: 8
+BRH: 4
+LH: 15
+LF: 20`
+    )
+    const output = parseLcov("randomPath")
+    expect(output).toEqual({})
+  })
+
+  it("fails to pass if FNF (number of functions) is missing", () => {
+    setupCoverageFile(
+      `TN:
+SF: some/file.ts
+FN: 1, func1
+FNDA: 1, func1
+FNH: 1
+BRF: 8
+BRH: 4
+LH: 15
+LF: 20
+end_of_record`
+    )
+    expect(() => parseLcov("randomPath")).toThrow()
+  })
+
+  it("can parse a correctly formatted lcov file with two records", () => {
+    setupCoverageFile(
+      `TN:
+SF: some/file1.ts
+FN: 1, func1
+FNDA: 1, func1
+FNH: 1
+FNF: 1
+BRF: 8
+BRH: 4
+LH: 15
+LF: 20
+end_of_record
+SF: some/file2.ts
+FN: 1, func1
+FNDA: 1, func1
+FN: 1, func2
+FNDA: 1, func2
+FNH: 1
+FNF: 2
+BRF: 8
+BRH: 4
+LH: 15
+LF: 20
+end_of_record`
+    )
+    const output = parseLcov("randomPath")
+    expect(output).toEqual({
+      "some/file1.ts": {
+        lines: { total: 20, covered: 15, skipped: 5, pct: 75 },
+        functions: { total: 1, covered: 1, skipped: 0, pct: 100 },
+        statements: { total: 20, covered: 15, skipped: 5, pct: 75 },
+        branches: { total: 8, covered: 4, skipped: 4, pct: 50 },
+      },
+      "some/file2.ts": {
+        lines: { total: 20, covered: 15, skipped: 5, pct: 75 },
+        functions: { total: 2, covered: 1, skipped: 1, pct: 50 },
+        statements: { total: 20, covered: 15, skipped: 5, pct: 75 },
+        branches: { total: 8, covered: 4, skipped: 4, pct: 50 },
+      },
+    })
+  })
+
+  it("throws an error when the file doesn't exist", () => {
+    setupCoverageFile(undefined)
+    expect(() => parseLcov("randomPath")).toThrow()
+  })
 })
