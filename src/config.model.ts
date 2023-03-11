2c02fe63f967d34f05e5f1edc8f07538b569967e
Samitha <samitha@Samithas-MacBook-Pro---Calcey.local>
2023-03-11 16:34:06 +0530

    feat: added useAbsolutePath prop required for lcov reports

--- src/config.model.ts
+++ src/config.model.ts
@@ -1,37 +1,38 @@
-export type ReportFileSet = "created" | "modified" | "createdOrModified" | "all"
-export type ReportMode = "fail" | "warn" | "message"
+export type ReportFileSet = 'created' | 'modified' | 'createdOrModified' | 'all';
+export type ReportMode = 'fail' | 'warn' | 'message';
 export type SortMethod =
-  | "alphabetically"
-  | "least-coverage"
-  | "most-coverage"
-  | "largest-file-size"
-  | "smallest-file-size"
-  | "uncovered-lines"
+    | 'alphabetically'
+    | 'least-coverage'
+    | 'most-coverage'
+    | 'largest-file-size'
+    | 'smallest-file-size'
+    | 'uncovered-lines';
 
-export type SourceType = "json-summary" | "lcov"
+export type SourceType = 'json-summary' | 'lcov';
 export interface SourcePathExplicit {
-  path: string
-  type: SourceType
+    path: string;
+    type: SourceType;
 }
-export type SourcePath = string | SourcePathExplicit
+export type SourcePath = string | SourcePathExplicit;
 
 export interface CoverageThreshold {
-  statements: number
-  branches: number
-  functions: number
-  lines: number
+    statements: number;
+    branches: number;
+    functions: number;
+    lines: number;
 }
 
 export interface Config {
-  customSuccessMessage?: string
-  customFailureMessage?: string
-  numberOfEntries: number
-  entrySortMethod: SortMethod
-  coveragePath?: SourcePath
-  coveragePaths: SourcePath[]
-  reportFileSet: ReportFileSet
-  threshold: CoverageThreshold
-  reportMode: ReportMode
+    customSuccessMessage?: string;
+    customFailureMessage?: string;
+    numberOfEntries: number;
+    useAbsolutePath: boolean;
+    entrySortMethod: SortMethod;
+    coveragePath?: SourcePath;
+    coveragePaths: SourcePath[];
+    reportFileSet: ReportFileSet;
+    threshold: CoverageThreshold;
+    reportMode: ReportMode;
 }
 
 /**
@@ -40,23 +41,27 @@ export interface Config {
  * @returns A complete configuration
  */
 export function makeCompleteConfiguration(config?: Partial<Config>): Config {
-  const defaults: Config = {
-    coveragePaths: [],
-    reportFileSet: "all",
-    reportMode: "message",
-    entrySortMethod: "alphabetically",
-    numberOfEntries: 10,
-    threshold: {
-      statements: 100,
-      branches: 100,
-      functions: 100,
-      lines: 100,
-    },
-  }
+    const defaults: Config = {
+        coveragePaths: [],
+        reportFileSet: 'all',
+        reportMode: 'message',
+        entrySortMethod: 'alphabetically',
+        numberOfEntries: 10,
+        useAbsolutePath: true,
+        threshold: {
+            statements: 100,
+            branches: 100,
+            functions: 100,
+            lines: 100
+        }
+    };
 
-  const combined = config ? { ...defaults, ...config } : defaults
-  const coveragePath = combined.coveragePath ? combined.coveragePath : "./coverage/coverage-summary.json"
-  const coveragePaths = combined.coveragePaths.length === 0 ? [coveragePath] : combined.coveragePaths
-  delete combined.coveragePath
-  return { ...combined, coveragePaths }
+    const combined = config ? { ...defaults, ...config } : defaults;
+    const coveragePath = combined.coveragePath
+        ? combined.coveragePath
+        : './coverage/coverage-summary.json';
+    const coveragePaths =
+        combined.coveragePaths.length === 0 ? [coveragePath] : combined.coveragePaths;
+    delete combined.coveragePath;
+    return { ...combined, coveragePaths };
 }
